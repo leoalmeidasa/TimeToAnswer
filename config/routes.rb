2c1935967123780aced0039145fe87475ca88f43
Leonardo <leoalmeidasa@gmail.com>
2022-01-24 19:11:42 -0300

    refactor: remove profile backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
 
-  root to: 'welcome#index'
+  root to: 'site/welcome#index'
 end
