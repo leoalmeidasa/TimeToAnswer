dcf7104348f3170b12cc2eaecde8aa81dfc29a42
Leonardo <leoalmeidasa@gmail.com>
2022-01-24 19:20:52 -0300

    feat: add user backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
