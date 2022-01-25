65e3ea13d3a0b2df1de7d5f4c91fbec6882ba9cf
Leonardo <leoalmeidasa@gmail.com>
2022-01-25 18:23:11 -0300

    feat: add themes gentelella and sbadmin

---
+++ app/javascript/packs/admins_backoffice.js
@@ -0,0 +1,6 @@
+/*
+*= require /bootstrap/dist/js/bootstrap.bundle.min.js
+*= require /jquery/dist/jquery.min.js
+*= require /jquery.easing/jquery.easing.min.js
+*= require sb-admin-2.js
+*/
---
+++ app/javascript/packs/users_backoffice.js
@@ -0,0 +1,8 @@
+/*
+*= require jquery-2.2.3/dist/jquery.js
+*= require /bootstrap/dist/js/bootstrap.bundle.min.js
+*= require /fastclick/lib/fastclick.js
+*= require /nprogress/nprogress.js
+*= require /malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js
+*= require custom.js
+*/
