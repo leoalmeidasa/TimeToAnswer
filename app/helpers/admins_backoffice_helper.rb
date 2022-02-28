1e7fb815fbe5aaec2557c42da7b3ec139b3bd24f
Leonardo <leoalmeidasa@gmail.com>
2022-02-28 10:55:26 -0300

    feat: add answer task

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,7 +1,7 @@
 module AdminsBackofficeHelper
   def translate_attribute(object =  nil, method = nil)
     if object && method
-      object.model.human_attribute_name(method)
+      object.human_attribute_name(method)
     else
       "Informe os parâmetros corretamente !"
     end
